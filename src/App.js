fe29599ed7bcb854828c746ed6266ecd932e12d7
Aleksandr Shatskikh <work@errand.ru>
2022-04-13 21:40:42 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Hex2RGB from "./components/Hex2RGB";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App--wrapper">
+      <Hex2RGB />
     </div>
   );
 }
